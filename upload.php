0d0d2f7b6c7291588b6c0b9102787fbfb2c88954
stephaniequiceno <stephaniequiceno789@gmail.com>
2023-11-18 17:26:19 +0000

    Added

---
+++ upload.php
@@ -0,0 +1,130 @@
+<?php
+if(isset($_POST["submit"])){
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if($check !== false){
+        $image = $_FILES['image']['tmp_name'];
+        $imgContent = addslashes(file_get_contents($image));
+
+        /*
+         * Insert image data into database
+         */
+        
+        //DB details
+        $dbHost     = 'localhost';
+        $dbUsername = 'root';
+        $dbPassword = '';
+        $dbName     = 'tutorial';
+        
+        //Create connection and select DB
+        $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+        
+        // Check connection
+        if($db->connect_error){
+            die("Connection failed: " . $db->connect_error);
+        }
+        
+        $dataTime = date("Y-m-d H:i:s");
+        
+        //Insert image content into database
+        $insert = $db->query("INSERT into images (image, created) VALUES ('$imgContent', '$dataTime')");
+        if($insert){
+            echo "File uploaded successfully.";
+        }else{
+            echo "File upload failed, please try again.";
+        } 
+    }else{
+        echo "Please select an image file to upload.";
+    }
+    if(!empty($_GET['id'])){
+        $result = $db->query("SELECT image FROM images WHERE id = {$_GET['id']}");
+    
+        if($result->num_rows > 0){
+            $imgData = $result->fetch_assoc();
+        
+            //Render image
+            header("Content-type: image/jpg"); 
+            echo $imgData['image']; 
+        }else{
+            echo 'Image not found...';
+        }
+    }
+}
+?>
+
+<?php
+
+  require_once 'connection.php';
+
+  $sql = "SELECT * FROM product";
+  $all_product = $conn->query($sql);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="font/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Ecommerce Website</title>
+</head>
+<body>
+    <?php
+      include_once 'header.php';
+
+   ?>
+
+   <main>
+       <?php
+          while($row = mysqli_fetch_assoc($all_product)){
+       ?>
+       <div class="card">
+           <div class="image">
+               <img src="<?php echo $row["product_image"]; ?>" alt="">
+           </div>
+           <div class="caption">
+               <p class="rate">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+               </p>
+               <p class="product_name"><?php echo $row["product_name"];  ?></p>
+               <p class="price"><b>$<?php echo $row["price"]; ?></b></p>
+               <p class="discount"><b><del>$<?php echo $row["discount"]; ?></del></b></p>
+           </div>
+           <button class="add" data-id="<?php echo $row["product_id"];  ?>">Add to cart</button>
+       </div>
+       <?php
+
+          }
+     ?>
+   </main>
+   <script>
+       var product_id = document.getElementsByClassName("add");
+       for(var i = 0; i<product_id.length; i++){
+           product_id[i].addEventListener("click",function(event){
+               var target = event.target;
+               var id = target.getAttribute("data-id");
+               var xml = new XMLHttpRequest();
+               xml.onreadystatechange = function(){
+                   if(this.readyState == 4 && this.status == 200){
+                       var data = JSON.parse(this.responseText);
+                       target.innerHTML = data.in_cart;
+                       document.getElementById("badge").innerHTML = data.num_cart + 1;
+                   }
+               }
+
+               xml.open("GET","connection.php?id="+id,true);
+               xml.send();
+            
+           })
+       }
+
+   </script>
+</body>
+</html>
